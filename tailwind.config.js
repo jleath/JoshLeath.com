f9de831b75e97666ef1bc9fa32758d7cab4024da
Joshua Leath <lawnfurniture@icloud.com>
2022-04-29 17:58:07 -0400

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ['Source Sans Pro', 'sans-serif'],
+    },
+    minHeight: {
+      'project-card': '375px',
+    },
+    colors: {
+      'chimera-green': '#66ac9a',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
